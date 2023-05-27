38e9685eca3d7497d3a9734d03b248d88bc47583
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-05-27 15:00:38 +0530

    Healthify-App backend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
 
+    return res.json({ "Welcome": ` to my Backend Software for the Healthify Web App ` });
+
+});
 router.use('/api', require('./users'));
 router.use('/api', require('./auth'));
 router.use('/api', require('./yoga'));
